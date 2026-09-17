456f706d9043d25c90ea50e03436fbe3b7cdae3c
AIL-PC-307-Windows <aakash.gupta@argusoft.com>
2026-09-17 11:40:39 +0530

    Tested the router With a dummy test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,16 @@
 import { Component, signal, WritableSignal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Child } from './child/child';
 import { DisplayCount } from './display-count/display-count';
 import { ControlCount } from './control-count/control-count';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [Child, DisplayCount, ControlCount],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  users = signal(["Hello", "Hi", "How", "Are", "You?"]);
-  newUser: WritableSignal<string> = signal("");
-  selectedWord = signal('');
 
-  addNewUser() {
-    this.users.update((data) => [...data, this.newUser()]);
-    this.newUser.set("");
-  }
-
-  selectedWordFunc(word: string) {
-    console.log(word);
-    this.selectedWord.set(word);
-  }
-
-  deleteWordFunc(word: string) {
-     this.users.update(data => data.filter(item => item!=word))
-  }
 }
